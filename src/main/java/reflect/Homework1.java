efa90fe1fa0e14be9b56e34e4012ab976c74d8d1
Ferenc Tóth <tothferenc626@gmail.com>
2021-03-15 15:25:29 +0100

    .

---
+++ src/main/java/reflect/Homework1.java
@@ -0,0 +1,100 @@
+package reflect;
+
+import java.lang.reflect.*;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.IntSummaryStatistics;
+
+public class Homework1 {
+
+    /**
+     * Prints the declared methods of java.lang.String sorted by name.
+     */
+    public void streamPipeline1() {
+        // TODO
+    }
+
+    /**
+     *  Prints all distinct names of the declared methods of java.lang.String sorted alphabetically.
+     */
+    public void streamPipeline2() {
+        // TODO
+    }
+
+    /**
+     * Prints the declared methods of java.lang.String with two or more parameters whose parameters are all of the same type, sorted by name.
+     */
+    public void streamPipeline3() {
+        // TODO
+    }
+
+    /**
+     * Prints all distinct return types of the declared methods of java.lang.String sorted alphabetically.
+     */
+    public void streamPipeline4() {
+        // TODO
+    }
+
+    /**
+     * Prints the declared methods of java.lang.String with at least one boolean parameter, sorted by name.
+     */
+    public void streamPipeline5() {
+        // TODO
+    }
+
+    /**
+     * Prints the declared methods of java.lang.String whose parameters are all of type int, sorted by name.
+     */
+    public void streamPipeline6() {
+        // TODO
+    }
+
+    /**
+     * Returns the longest name of the declared methods of java.lang.String.
+     */
+    public String streamPipeline7() {
+        // TODO
+        return null;
+    }
+
+    /**
+     *  Returns the longest one from the names of the public declared methods of java.lang.String.
+     */
+    public String streamPipeline8() {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Returns summary statistics about the number of parameters for the declared methods of java.lang.String.
+     */
+    public IntSummaryStatistics streamPipeline9() {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Returns the maximum number of parameters accepted by the declared methods of java.lang.String.
+     */
+    public int streamPipeline10() {
+        // TODO
+        return 0;
+    }
+
+    /**
+     * Returns the declared method of java.lang.String with the most number of parameters.
+     */
+    public Method streamPipeline11() {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Prints all distinct parameter types of the declared methods of java.lang.String sorted alphabetically.
+     */
+    public void streamPipeline12() {
+        // TODO
+    }
+
+}
