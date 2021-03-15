82596db62ae053e236dbfdeeb7be6c621ce3639b
Ferenc Tóth <tothferenc626@gmail.com>
2021-03-15 18:47:36 +0100

    Modified Homework1.java

--- src/main/java/reflect/Homework1.java
+++ src/main/java/reflect/Homework1.java
@@ -12,89 +12,85 @@ public class Homework1 {
      * Prints the declared methods of java.lang.String sorted by name.
      */
     public void streamPipeline1() {
-        // TODO
+	Arrays.stream(java.lang.String.class.getDeclaredMethods()).sorted(Comparator.comparing(s -> s.getName())).forEach(System.out::println);
     }
 
     /**
      *  Prints all distinct names of the declared methods of java.lang.String sorted alphabetically.
      */
     public void streamPipeline2() {
-        // TODO
+        Arrays.stream(java.lang.String.class.getDeclaredMethods()).map(s -> s.getName()).distinct().sorted().forEach(System.out::println);
     }
 
     /**
      * Prints the declared methods of java.lang.String with two or more parameters whose parameters are all of the same type, sorted by name.
      */
     public void streamPipeline3() {
-        // TODO
+        Arrays.stream(java.lang.String.class.getDeclaredMethods()).filter(s -> s.getParameterCount() >= 2).filter(s -> Arrays.stream(s.getParameterTypes()).distinct().count() == 1).sorted(Comparator.comparing(s -> s.getName())).forEach(System.out::println);
+
     }
 
     /**
      * Prints all distinct return types of the declared methods of java.lang.String sorted alphabetically.
      */
     public void streamPipeline4() {
-        // TODO
+        Arrays.stream(java.lang.String.class.getDeclaredMethods()).map(s -> s.getReturnType()).distinct().sorted(Comparator.comparing(s -> s.getTypeName())).forEach(System.out::println);
     }
 
     /**
      * Prints the declared methods of java.lang.String with at least one boolean parameter, sorted by name.
      */
     public void streamPipeline5() {
-        // TODO
+        Arrays.stream(java.lang.String.class.getDeclaredMethods()).filter(s -> Arrays.stream(s.getParameterTypes()).anyMatch(z -> z == boolean.class)).sorted(Comparator.comparing(s -> s.getName())).forEach(System.out::println);
     }
 
     /**
      * Prints the declared methods of java.lang.String whose parameters are all of type int, sorted by name.
      */
     public void streamPipeline6() {
-        // TODO
+        Arrays.stream(java.lang.String.class.getDeclaredMethods()).filter(s -> Arrays.stream(s.getParameterTypes()).allMatch(z -> z == int.class) && s.getParameterCount() != 0).sorted(Comparator.comparing(s -> s.getName())).forEach(System.out::println);
     }
 
     /**
      * Returns the longest name of the declared methods of java.lang.String.
      */
     public String streamPipeline7() {
-        // TODO
-        return null;
+        return Arrays.stream(java.lang.String.class.getDeclaredMethods()).map(s -> s.getName()).max(Comparator.comparingInt(s -> s.length())).get();
     }
 
     /**
      *  Returns the longest one from the names of the public declared methods of java.lang.String.
      */
     public String streamPipeline8() {
-        // TODO
-        return null;
+        return Arrays.stream(java.lang.String.class.getDeclaredMethods()).filter(s -> s.getModifiers() == java.lang.reflect.Modifier.PUBLIC).map(s -> s.getName()).max(Comparator.comparingInt(s -> s.length())).get();
     }
 
     /**
      * Returns summary statistics about the number of parameters for the declared methods of java.lang.String.
      */
     public IntSummaryStatistics streamPipeline9() {
-        // TODO
-        return null;
+        return Arrays.stream(java.lang.String.class.getDeclaredMethods()).mapToInt(s -> s.getParameterCount()).summaryStatistics();
     }
 
     /**
      * Returns the maximum number of parameters accepted by the declared methods of java.lang.String.
      */
     public int streamPipeline10() {
-        // TODO
-        return 0;
+        return Arrays.stream(java.lang.String.class.getDeclaredMethods()).mapToInt(s -> s.getParameterCount()).max().getAsInt();
     }
 
     /**
      * Returns the declared method of java.lang.String with the most number of parameters.
      */
     public Method streamPipeline11() {
-        // TODO
-        return null;
+        return Arrays.stream(java.lang.String.class.getDeclaredMethods()).max(Comparator.comparingInt(s -> s.getParameterCount())).get();
     }
 
     /**
      * Prints all distinct parameter types of the declared methods of java.lang.String sorted alphabetically.
      */
     public void streamPipeline12() {
-        // TODO
+        Arrays.stream(java.lang.String.class.getDeclaredMethods()).flatMap(s -> Arrays.stream(s.getParameterTypes())).distinct().sorted(Comparator.comparing(z -> z.getName())).forEach(System.out::println);
     }
 
 }
